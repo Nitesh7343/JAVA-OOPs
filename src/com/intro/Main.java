76fa98d1e7f9ed79e17ebb42efcf3b391c7c1cc2
Nitesh <niteshsingh7343@gmail.com>
2025-12-24 16:57:17 +0530

    Access modifiers - Public, Private , Default.

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -1,7 +1,8 @@
 package com.intro;
 
+import com.Access.A;
 
-class Student {
+class Student extends A {
     String name;
     int rollNo;
     double marks;
@@ -11,12 +12,14 @@ class Student {
     }
 
     Student () {
+        super();
         this.name = "Nitesh";
         this.rollNo = 39;
         this.marks = 98;
     }
 
     Student(String name, int rollNo,double marks) {
+        super();
         this.name = name;
         this.rollNo = rollNo;
         this.marks = marks;
@@ -24,9 +27,14 @@ class Student {
 
 
     Student(Student other) {
+        super();
         this.name = other.name;
         this.rollNo = other.rollNo;
         this.marks = other.marks;
+    }
+
+    void show() {
+        System.out.println("by class Student "+str);
     }
 
     void changeName(String newName) {
@@ -38,25 +46,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student stu1 = new Student();
-        Student stu2 = new Student("Ashu",02,99);
-        Student frontend = new Student(stu2);
-        Student backend = new Student(stu1);
-
-        System.out.println(stu1.name);
-        System.out.println(stu2.name);
-        System.out.println(frontend.name);
-        System.out.println(backend.name);
-
-        System.out.println("-------------------------------------------------");
-
-        Student me = stu1;
+//        Student stu1 = new Student();
+//        Student stu2 = new Student("Ashu",02,99);
+//        Student frontend = new Student(stu2);
+//        Student backend = new Student(stu1);
+//
+//        System.out.println(stu1.name);
+//        System.out.println(stu2.name);
+//        System.out.println(frontend.name);
+//        System.out.println(backend.name);
+//
+//        System.out.println("-------------------------------------------------");
+//
+//        Student me = stu1;
 
 //        System.out.println(stu1.name);
 //        System.out.println(me.name);
 //        me.name = "paandaa";
 //        System.out.println(stu1.name);
 //        System.out.println(me .name);
+
+        Student obj = new Student();
+
+        A objA = new A(39,"nitesh",true);
+
+//        System.out.println(obj.num); // can not access due to private
+        //to access use getter
+        System.out.println("by class Student "+obj.getNum());
+
+//        System.out.println(obj.access); //access is dafault can not be accessed outside the pakage
+
+//        System.out.println(obj.name); // can not access bacause the name is protected. so can be accessed by ony in subclass , as the show mehtode in the subclass can access it.
+        obj.show();
+
+        System.out.println(objA.getNum()); //private getter
+        System.out.println(objA.showStr()); //protected getter
+        System.out.println(objA.access()); //default getter
 
 
 
