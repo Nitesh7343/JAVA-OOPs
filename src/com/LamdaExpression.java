f138a360ed0cd7717f0dfdba6d86df145617bfe8
Nitesh <niteshsingh7343@gmail.com>
2025-12-25 23:51:14 +0530

    My calculator using lambda function.

--- src/com/LamdaExpression.java
+++ src/com/LamdaExpression.java
@@ -4,17 +4,40 @@ import java.util.ArrayList;
 
 public class LamdaExpression {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 4;
-        ArrayList<Integer> lst = new ArrayList<>(0);
-        for(int i = 0 ;i < 10;i++) {
-            lst.add(i);
-        }
-        lst.forEach((item)-> System.out.println(item*2));
+//        ArrayList<Integer> lst = new ArrayList<>(0);
+//        for(int i = 0 ;i < 10;i++) {
+//            lst.add(i);
+//        }
+//        lst.forEach((item)-> System.out.println(item*2));
+
+        Operation sum = (a,b)->a+b;
+        Operation sub = (a,b)->a-b;
+        Operation pro = (a,b)->a*b;
+        Operation div = (a,b)->a/b;
+        Operation rem = (a,b)->a%b;
+        Operation pow = (a,b)->{
+            if(b == 0) return 1;
+            int i = 1;
+            int temp = a;
+            while(i != b) {
+                a *= temp;
+                i++;
+            }
+            return a;
+        };
+
+        LamdaExpression myCalc = new LamdaExpression();
+        int ans = myCalc.operate(2,2,pow);
+
+        System.out.println(ans);
+
+    }
+
+    private static int operate(int a, int b , Operation op) {
+        return op.operation(a,b);
     }
 }
 
 interface Operation {
-    public int sum(int a , int b);
-    public int sub(int a, int b);
+    public int operation(int a , int b);
 }
